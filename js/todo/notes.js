80a102c2a7b25d221d5be2037a5c20351982e007
Kyle <kyle.mirley@gmail.com>
2017-10-31 14:38:33 -0400

    updated notes

--- js/todo/notes.js
+++ js/todo/notes.js
@@ -7,11 +7,20 @@ function Notes(x,y) {
   this.r = random(125);
   this.g = random(125);
   this.b = random(125);
+  this.msg = "";
 
 
   this.draw = function(){
     fill(this.r,this.g,this.b);
     ellipseMode(RADIUS);
     ellipse(this.body.position.x,this.body.position.y,this.w,this.h);
+  }
+
+  this.getColor = function(){
+    return [this.r,this.g,this.b];
+  }
+
+  this.setMessage = function(message){
+    this.msg = message;
   }
 }
